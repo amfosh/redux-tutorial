f2f8d4b9c4d4ccdb11d34c08670ab0019e7a3bd0
amfosh <afasciocco@gmail.com>
2020-07-04 13:49:27 -0400

    rendering some items

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from './Footer.js'
+// import AddTodo from '../containers/AddTodo'
+// import VisibleTodoList from '../containers/VisibleTodoList'
+
+
+
+const App = () => (
+  <div>
+      Hello
+    {/* <AddTodo />
+    <VisibleTodoList /> */}
+    <Footer />
+  </div>
+)
+
+export default App;
