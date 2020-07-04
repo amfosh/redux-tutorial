8475be7fc3f54ee762bd1d07da76510eaaf890d8
amfosh <afasciocco@gmail.com>
2020-07-04 19:42:41 -0400

    all working

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Footer from './Footer.js'
-// import AddTodo from '../containers/AddTodo'
-// import VisibleTodoList from '../containers/VisibleTodoList'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
 
 
 
 const App = () => (
   <div>
       Hello
-    {/* <AddTodo />
-    <VisibleTodoList /> */}
+    <AddTodo />
+    <VisibleTodoList />
     <Footer />
   </div>
 )
